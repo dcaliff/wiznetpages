b4a5beb2b5513936270d781dec3950b54c3ccc0a
many <emanmomot@gmail.com>
2017-02-27 23:00:55 -0500

    fishing sucks

--- TemplateData/chatController.js
+++ TemplateData/chatController.js
@@ -12,6 +12,30 @@ var urlRegex = new RegExp(urlRegexString);
 var newLineRegexString = '\n';
 var newLineRegex = new RegExp(newLineRegexString, 'g');
 
+var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+
+var tagOrComment = new RegExp(
+    '<(?:'
+    // Comment body.
+    + '!--(?:(?:-*[^->])*--+|-?)'
+    // Special "raw text" elements whose content should be elided.
+    + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
+    + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+    // Regular name
+    + '|/?[a-z]'
+    + tagBody
+    + ')>',
+    'gi');
+
+function removeTags(html) {
+  var oldHtml;
+  do {
+    oldHtml = html;
+    html = html.replace(tagOrComment, '');
+  } while (html !== oldHtml);
+  return html.replace(/</g, '&lt;');
+}
+
 chatInput.onblur = function() {
    setTimeout(function() {
     chatInput.focus();
@@ -51,6 +75,8 @@ function sendChatMsg(msg) {
 }
 
 function recieveChatMsg(username, playerColor, msg, color, atPlayer) {
+	msg = removeTags(msg);
+	
 	var newChatLine = "";
 
 	//username:
